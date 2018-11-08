1291d9b35058e9f375c39449004e01180d7df63d
NEOWIZ\hgkim0905 <lacey0905@naver.com>
2018-11-08 17:16:45 +0900

    START

---
+++ Assets/Scripts/CGameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CGameManager : MonoBehaviour {
+
+
+
+	void Start ()
+    {
+        Debug.Log("Hello World!");
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
